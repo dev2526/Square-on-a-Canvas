433afe97b2ecfcfcb2d5b5e1ffb74c8759aab62c
dev2526 <devtandon45@gmail.com>
2024-11-17 19:28:03 +0530

    activity 1

---
+++ Square on a Canvas.py
@@ -0,0 +1,26 @@
+import turtle 
+
+turtle.Screen().bgcolor("red")
+sc=turtle.screen()
+sc.setup(400,300)
+
+
+turtle.title("welcome to python turtle")
+
+board=turtle.Turtle()
+
+
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+
+
+
+
+
+
+turtle.done()
+
